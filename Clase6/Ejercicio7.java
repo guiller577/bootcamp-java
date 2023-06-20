5ddcb28f8b391c59e465b577504c4b3ac0fd942a
Guillermina <44557728+guiller577@users.noreply.github.com>
2023-06-20 02:16:51 -0300

    Subiendo ejercicios 7 de la clase 6

---
+++ Clase6/Ejercicio7.java
@@ -0,0 +1,12 @@
+package Clase6;
+
+public class Ejercicio7 {
+    public static void main(String[] args){
+        int i=1;
+
+        while(i < 101){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
